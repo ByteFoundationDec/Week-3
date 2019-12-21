09b356929c844b496136285ef2c9d3da20573d20
Pradip Tivhale <pradiptivhale@gmail.com>
2019-12-21 11:26:50 +0530

    Add files via upload

---
+++ math_10.py
@@ -0,0 +1,15 @@
+def addition(arg1, arg2):
+	x1 = arg1 + arg2
+	return x1
+
+def product(arg3, arg4):
+	x2 = arg3*arg4
+	return x2
+
+def square(arg5):
+	x3 = arg5**2
+	return x3
+
+def cube(arg6):
+	x4 = arg6**3
+	return x4
---
+++ mylibrary.py
@@ -0,0 +1,4 @@
+def greeting(name='Aditya'):
+    print("Hello, " + name)
+    print("How are you?")
+
